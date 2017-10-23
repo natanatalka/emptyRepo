3c39e9f06ac97755bba3b29644da30e653935323
test <test@test.com>
2017-10-23 17:12:35 +0300

    [Studio] Add screen "window"

---
+++ client/src/pages/Window/TabletLandscape/Window.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-window-tabletlandscape',
+  templateUrl: 'Window.html'
+})
+export class Window_TabletLandscape extends Screen {
+}
