ad1afc5b05fecd2ccc2b0a7c6a02f91a4b57134d
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-23 17:09:29 +0300

    [Studio] Add screen "cup"

--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,4 +1,5 @@
 export * from './Blank/index';
 export * from './Flower_Apple/index';
+export * from './Flower_Cup/index';
 export * from './Flower_Fur/index';
 export * from './Flower_Login/index';
---
+++ client/src/pages/Flower/Cup/TabletLandscape/Cup.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-flower-cup_tabletlandscape',
+  templateUrl: 'Cup.html'
+})
+export class Flower_Cup_TabletLandscape extends Screen {
+}
