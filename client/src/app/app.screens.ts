4cbfae17fbf6ebe255dde0995c7279de250c0bad
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-23 16:37:56 +0300

    [Studio] Add screen "Login"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -2,14 +2,19 @@
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
 import { Blank_TabletLandscape } from '../pages/Blank/TabletLandscape/Blank';
+import { Login_TabletLandscape } from '../pages/Login/TabletLandscape/Login';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
-    Blank_TabletLandscape
+    Blank_TabletLandscape,
+    Login_TabletLandscape
   ];
   static mapping = {
     'Blank': {
       TabletLandscape: Blank_TabletLandscape
+    },
+    'Login': {
+      TabletLandscape: Login_TabletLandscape
     }
   }
 }
---
+++ client/src/pages/Login/TabletLandscape/Login.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-login-tabletlandscape',
+  templateUrl: 'Login.html'
+})
+export class Login_TabletLandscape extends Screen {
+}
