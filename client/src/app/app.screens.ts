43a023cd12bf94e3e6fa9f3372a6ab1721716f91
test <test@test.com>
2017-10-23 16:33:25 +0300

    [Studio] Add screen "Blank"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,9 +1,15 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
+import { Blank_TabletLandscape } from '../pages/Blank/TabletLandscape/Blank';
 export class Screens {
   static declarations: Page[] = [
-    MenuComponent
+    MenuComponent,
+    Blank_TabletLandscape
   ];
-  static mapping = {}
+  static mapping = {
+    'Blank': {
+      TabletLandscape: Blank_TabletLandscape
+    }
+  }
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,1 +1,1 @@
-export {};
+export * from './Blank/index';
