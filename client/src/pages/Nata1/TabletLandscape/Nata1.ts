470e37ae8b2eeaa4e8261f7de37de75ded429db4
test <test@test.com>
2017-10-24 13:26:56 +0300

    [Studio] Add screen "Nata1"

---
+++ client/src/pages/Nata1/TabletLandscape/Nata1.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-nata1-tabletlandscape',
+  templateUrl: 'Nata1.html'
+})
+export class Nata1_TabletLandscape extends Screen {
+}
