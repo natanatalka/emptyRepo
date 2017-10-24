b847dba99b1a18894c5152eb164fa94ac4f30c5c
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-24 13:37:09 +0300

    [Studio] Add screen "Screen1"

---
+++ client/src/pages/Screen1/TabletLandscape/Screen1.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-screen1-tabletlandscape',
+  templateUrl: 'Screen1.html'
+})
+export class Screen1_TabletLandscape extends Screen {
+}
