8718fee7fb17176940918b86f9a8b49456a2233c
test <test@test.com>
2017-10-24 13:20:13 +0300

    [Studio] Add screen "Test1"

--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -5,3 +5,4 @@ export * from './Flower_Cup/index';
 export * from './Flower_Fur/index';
 export * from './Flower_Login/index';
 export * from './Rain_1/index';
+export * from './Test1/index';
---
+++ client/src/pages/Test1/TabletLandscape/Test1.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-test1-tabletlandscape',
+  templateUrl: 'Test1.html'
+})
+export class Test1_TabletLandscape extends Screen {
+}
