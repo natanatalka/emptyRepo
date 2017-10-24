abfc02ff8f31b235a445246c9579f598411cf876
test <test@test.com>
2017-10-24 13:37:32 +0300

    [Studio] Delete screen "Screen1"

--- client/src/pages/Screen1/TabletLandscape/Screen1.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Screen } from 'app/screen';
-
-@Component({
-  selector: 'screen-screen1-tabletlandscape',
-  templateUrl: 'Screen1.html'
-})
-export class Screen1_TabletLandscape extends Screen {
-}
