c8d909c245f68f4271b8e3966555d9aee642e9f8
test <test@test.com>
2017-10-24 13:26:35 +0300

    [Studio] Delete screen "Nata1"

--- client/src/pages/Nata1/TabletLandscape/Nata1.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Screen } from 'app/screen';
-
-@Component({
-  selector: 'screen-nata1-tabletlandscape',
-  templateUrl: 'Nata1.html'
-})
-export class Nata1_TabletLandscape extends Screen {
-}
