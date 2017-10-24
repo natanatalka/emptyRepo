ced50a006aa7427ddcba154a4a5815f32d84d996
test <test@test.com>
2017-10-24 13:20:43 +0300

    [Studio] Delete screen "Test2"

--- client/src/mocks/Test2/index.ts
+++
@@ -1,2 +0,0 @@
-export let Test2 = {
-};
--- client/src/pages/Test2/TabletLandscape/Test2.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Screen } from 'app/screen';
-
-@Component({
-  selector: 'screen-test2-tabletlandscape',
-  templateUrl: 'Test2.html'
-})
-export class Test2_TabletLandscape extends Screen {
-}
