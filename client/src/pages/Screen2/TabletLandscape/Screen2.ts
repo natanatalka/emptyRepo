2fd8d2a081df0bfb9c326de6721f3678647e3892
test <test@test.com>
2017-10-24 13:36:36 +0300

    [Studio] Add screen "Screen2"

---
+++ client/src/pages/Screen2/TabletLandscape/Screen2.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-screen2-tabletlandscape',
+  templateUrl: 'Screen2.html'
+})
+export class Screen2_TabletLandscape extends Screen {
+}
