4a4be4a85c26ab2bbc4c4117581a61599a57b86f
test <test@test.com>
2017-10-24 13:31:32 +0300

    [Studio] Delete screen "222"

--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,4 +1,3 @@
-export * from './222/index';
 export * from './Cloud_Blank1/index';
 export * from './Clould_Blank1/index';
 export * from './Flower_Apple/index';
--- client/src/pages/222/TabletLandscape/222.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Screen } from 'app/screen';
-
-@Component({
-  selector: 'screen-222-tabletlandscape',
-  templateUrl: '222.html'
-})
-export class 222_TabletLandscape extends Screen {
-}
