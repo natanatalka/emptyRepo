9e896b11affc0ceda4c46fbb395f398f361890f3
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-24 13:31:48 +0300

    [Studio] Add screen "111"

---
+++ client/src/pages/111/TabletLandscape/111.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-111-tabletlandscape',
+  templateUrl: '111.html'
+})
+export class 111_TabletLandscape extends Screen {
+}
