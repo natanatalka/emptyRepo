35e3f05a1342a0324349bc62d8927a4ff4a3372d
test <test@test.com>
2017-10-24 13:38:13 +0300

    [Studio] Delete screen "Login"

--- client/src/pages/Login/TabletLandscape/Login.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Screen } from 'app/screen';
-
-@Component({
-  selector: 'screen-login-tabletlandscape',
-  templateUrl: 'Login.html'
-})
-export class Login_TabletLandscape extends Screen {
-}
